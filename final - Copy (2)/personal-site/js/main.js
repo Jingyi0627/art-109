05540fc6058c00fe64e386f392855a8f1c014fb7
Jing <jingyi.wang01@sjsu.edu>
2026-04-12 11:44:34 -0700

    1

---
+++ final - Copy (2)/personal-site/js/main.js
@@ -0,0 +1,13 @@
+console.log("Hello js");
+
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navLinks = document.querySelector('.nav-links');
+
+    menuToggle.addEventListener('click', () => {
+        navLinks.classList.toggle('active');
+    });
+});
+
